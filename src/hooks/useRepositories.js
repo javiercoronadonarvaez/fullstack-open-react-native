5861e8119d3356335fcabb0c2623955b8eed790f
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-22 12:12:53 -0600

    Finished Exercise 10.11

--- src/hooks/useRepositories.js
+++ src/hooks/useRepositories.js
@@ -1,23 +1,26 @@
 import { useState, useEffect } from "react";
+import { useQuery } from "@apollo/client";
+import { GET_REPOSITORIES } from "../graphql/queries";
 
 const useRepositories = () => {
   const [repositories, setRepositories] = useState();
-  const [loading, setLoading] = useState(false);
+  //const [loading, setLoading] = useState(false);
+  const { data, loading } = useQuery(GET_REPOSITORIES, {
+    fetchPolicy: "cache-and-network",
+    // Other options
+  });
 
   const fetchRepositories = async () => {
-    setLoading(true);
-
     // Replace the IP address part with your own IP address!
-    const response = await fetch("http://192.168.0.228:5000/api/repositories");
-    const json = await response.json();
-
-    setLoading(false);
-    setRepositories(json);
+    const fetchedRepositories = data.repositories;
+    setRepositories(fetchedRepositories);
   };
 
   useEffect(() => {
-    fetchRepositories();
-  }, []);
+    if (data) {
+      fetchRepositories();
+    }
+  }, [data]);
 
   return { repositories, loading, refetch: fetchRepositories };
 };
