df7efbaad27cbbae8558cdea6ab30ad69ef77191
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-22 16:08:18 -0600

    Set up configuration for exercises 10.15 and 10.16

--- App.js
+++ App.js
@@ -6,6 +6,7 @@ import Constants from "expo-constants";
 import Main from "./src/components/Main";
 import createApolloClient from "./src/utils/apolloClient";
 import AuthStorage from "./src/utils/authStorage";
+import AuthStorageContext from "./src/contexts/AuthStorageContext";
 
 const authStorage = new AuthStorage();
 const apolloClient = createApolloClient(authStorage);
@@ -16,7 +17,9 @@ const App = () => {
     <>
       <NativeRouter>
         <ApolloProvider client={apolloClient}>
-          <Main />
+          <AuthStorageContext.Provider value={authStorage}>
+            <Main />
+          </AuthStorageContext.Provider>
         </ApolloProvider>
       </NativeRouter>
       <StatusBar style="auto" />
--- src/hooks/useSignIn.js
+++ src/hooks/useSignIn.js
@@ -1,7 +1,11 @@
 import { useMutation } from "@apollo/client";
 import { SIGN_IN } from "../graphql/mutations";
 
+import useAuthStorage from "./useAuthStorage";
+
 const useSignIn = () => {
+  const authStorage = useAuthStorage();
+  console.log("Storage Context", authStorage);
   const [authenticate, token] = useMutation(SIGN_IN);
 
   const signIn = async ({ username, password }) => {
