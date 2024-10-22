28578b9a052faba95a6d4578c093551694e2880e
javiercoronadonarvaez <j.coronado.narvaez@accenture.com>
2024-10-22 16:02:12 -0600

    Updating app first with authStorage in App.js

--- App.js
+++ App.js
@@ -5,7 +5,10 @@ import Constants from "expo-constants";
 
 import Main from "./src/components/Main";
 import createApolloClient from "./src/utils/apolloClient";
-const apolloClient = createApolloClient();
+import AuthStorage from "./src/utils/authStorage";
+
+const authStorage = new AuthStorage();
+const apolloClient = createApolloClient(authStorage);
 
 const App = () => {
   console.log(Constants.expoConfig);
